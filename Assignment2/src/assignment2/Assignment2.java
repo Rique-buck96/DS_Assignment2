dd3ca58f1556458b8d829e7e8f77848ad7cd32fc
Enrique Buck <rique.buck@gmail.com>
2016-04-26 17:30:56 +0800

    Initial Commit
    Bubble Sort

---
+++ Assignment2/src/assignment2/Assignment2.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author Enrique
+ */
+public class Assignment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int intArray[] = new int[]{5,90,35,45,150,3,9,11,80,50,75,12,41,100,112,95,32,64,16,8};
+               
+                //print array before sorting using bubble sort algorithm
+                System.out.println("Array Before Bubble Sort");
+                for(int i=0; i < intArray.length; i++){
+                        System.out.print(intArray[i] + " ");
+                }
+               
+                //sort an array using bubble sort algorithm
+                BubbleSort(intArray);
+               
+                System.out.println("");
+               
+                //print array after sorting using bubble sort algorithm
+                System.out.println("Array After Bubble Sort");
+                for(int i=0; i < intArray.length; i++){
+                        System.out.print(intArray[i] + " ");
+                }
+    }
+    
+     public static void BubbleSort( int [ ] num )
+    {
+     int j;
+     boolean flag = true;   // set flag to true to begin first pass
+     int temp;   //holding variable
+
+     while ( flag )
+     {
+            flag= false;    //set flag to false awaiting a possible swap
+            for( j=0;  j < num.length -1;  j++ )
+            {
+                   if ( num[ j ] > num[j+1] )   // sort in ascending order
+                   {
+                           temp = num[ j ];                //swap elements
+                           num[ j ] = num[ j+1 ];
+                           num[ j+1 ] = temp;
+                          flag = true;              //shows a swap occurred  
+                  } 
+            } 
+      } 
+    } 
+}
+    
+   
